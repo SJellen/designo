1cbbf79d3f6d640800acdd2b6ca1032135c91a05
SJellen <sjellen@me.com>
2021-09-01 14:35:58 -0400

    work on footer section

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,24 @@
 import styles from '../styles/Footer.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer() {
 
     return (
         <header className={styles.container}>
-            <h1>footer</h1>
+            <div className={styles.topContainer}>
+                <div className={styles.footerLogo}>
+                    <Image src="/assets/shared/desktop/logo-light.png" alt="open menu icon" width="404" height="54" />
+                </div>
+                <nav className={styles.nav}>
+                    <Link href="/about" ><a >OUR COMPANY</a></Link>
+                    <Link href="/locations" ><a >LOCATIONS</a></Link>
+                    <Link href="/contact" ><a >CONTACT</a></Link>
+                </nav>
+            </div>
+            <div>
+
+            </div>
         </header>
     )
 }
