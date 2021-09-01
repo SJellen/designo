9095b1e2c57594ac9061d81a5b449c2507865080
SJellen <sjellen@me.com>
2021-09-01 13:47:14 -0400

    added assets and begin header layout

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,19 @@
 import styles from '../styles/Header.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
 
     return (
         <header className={styles.container}>
-            <h1>The header</h1>
+        <div className={styles.headerLogo}>
+            <Image src="/assets/shared/desktop/logo-dark.png" alt="open menu icon" width="404" height="54" />
+        </div>
+        <nav className={styles.nav}>
+            <Link href="/about" ><a >OUR COMPANY</a></Link>
+            <Link href="/locations" ><a >LOCATIONS</a></Link>
+            <Link href="/contact" ><a >CONTACT</a></Link>
+        </nav>
         </header>
     )
 }
