9bbe31267249f5a10e6e36cb1317a2fa631a5d55
SJellen <sjellen@me.com>
2021-09-03 14:41:22 -0400

    background image set on getintouch

--- components/GetInTouch.js
+++ components/GetInTouch.js
@@ -1,11 +1,14 @@
 import styles from "../styles/GetInTouch.module.scss"
-
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function GetInTouch() {
 
     return (
         <div className={styles.container}>
+               
             <div className={styles.flexContainer}>
+            
                 <div className={styles.leftContainer}>
 
                 </div>
