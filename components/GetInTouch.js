cf143eabc598a97c59d3e70760b8acad04509ca2
SJellen <sjellen@me.com>
2021-09-03 15:06:01 -0400

    Get in touch section set

--- components/GetInTouch.js
+++ components/GetInTouch.js
@@ -6,14 +6,15 @@ export default function GetInTouch() {
 
     return (
         <div className={styles.container}>
-               
-            <div className={styles.flexContainer}>
-            
+            <div className={styles.flexContainer}>  
                 <div className={styles.leftContainer}>
-
+                    <h1>Let’s talk about your project</h1>
+                    <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
                 </div>
                 <div className={styles.rightContainer}>
-
+                    <div>
+                       <Link href="/contact"><a>GET IN TOUCH</a></Link> 
+                    </div>
                 </div>
             </div>
         </div>
