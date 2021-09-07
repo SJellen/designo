6cb3eb4b3379a6691d813d92aace4435922911c5
SJellen <sjellen@me.com>
2021-09-07 12:40:54 -0400

    setup on Home/Hero section

---
+++ components/Home/Hero.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/Home/Hero.module.scss'
+
+export default function Hero() {
+
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/Home/Home.module.scss'
+import Hero from '../components/Home/Hero'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>home</h1>
+      <Hero />
     </div>
   )
 }
