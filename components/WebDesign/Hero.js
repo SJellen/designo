880bc00008218205ff9ed3c986617fd2b4a5a16c
SJellen <sjellen@me.com>
2021-09-11 11:01:33 -0400

    set up on webDesign Hero

---
+++ components/WebDesign/Hero.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/WebDesign/Hero.module.scss'
+
+export default function Hero() {
+
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
--- pages/webdesign.js
+++ pages/webdesign.js
@@ -1,10 +1,11 @@
 import styles from '../styles/WebDesign/WebDesign.module.scss'
+import Hero from '../components/WebDesign/Hero'
 
 export default function WebDesign() {
 
     return (
         <div className={styles.container}>
-            web design
+            <Hero />
         </div>
     )
 }
