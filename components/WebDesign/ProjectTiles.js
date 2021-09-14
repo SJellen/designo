12769b99233058ebefb35ad3149c7292699ac03e
SJellen <sjellen@me.com>
2021-09-14 12:13:04 -0400

    layout on webdesign tiles

--- components/WebDesign/ProjectTiles.js
+++ components/WebDesign/ProjectTiles.js
@@ -1,11 +1,26 @@
 
 import styles from '../../styles/ProjectTile.module.scss'
+import webData from '../../data/webData.json'
+import Image from 'next/image'
 
 export default function ProjectTiles() {
 
+    const webTiles = webData.map((item) => (
+        <div key={item.id} className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src={`${item.image}`} alt="speaker" width="700" height="640"  className={styles.img}/>
+            </div>
+            <div className={styles.bottomContainer}>
+                <h1>{item.name}</h1>
+                <p>{item.description}</p>
+
+            </div>
+        </div>
+    ))
+
     return (
         <div>
-            tiles
+            {webTiles}
         </div>
     )
 }
