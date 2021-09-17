edc7abc07d26802a5b13083cc33241de3f8665b2
SJellen <sjellen@me.com>
2021-09-17 12:59:41 -0400

    Contact Hero section layout

---
+++ components/Contact/Hero.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/Contact/Hero.module.scss'
+
+export default function About() {
+
+    return (
+        <div className={styles.container}>
+           
+        </div>
+    )
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,11 @@
 import styles from '../styles/Contact/Contact.module.scss'
+import Hero from '../components/Contact/Hero'
 
 export default function Contact() {
 
     return (
         <div className={styles.container}>
-            Contact
+            <Hero />
         </div>
     )
 }
