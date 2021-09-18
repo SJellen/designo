28cc9246d691a97964c9f798c264e0216f1a2666
SJellen <sjellen@me.com>
2021-09-18 12:42:49 -0400

    linked LocationNav and location

--- components/LocationNav.js
+++ components/LocationNav.js
@@ -1,29 +1,36 @@
 import styles from '../styles/LocationNav.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function LocationNav() {
 
     return (
         <div className={styles.container}>
             <div className={styles.box}>
-                <div className={styles.topContainer}><Image src="/assets/home/desktop/illustration-passionate.svg" alt="open menu icon" width="202" height="202" /></div>
+                <div className={styles.topContainer}><Image src="/assets/shared/desktop/illustration-canada.svg" alt="location icon" width="202" height="202" /></div>
                 <div className={styles.bottomContainer}>
                     <h3>CANADA</h3>
-                    <a>SEE LOCATION</a>
+                    <div>
+                       <Link href="/locations"><a>SEE LOCATION</a></Link> 
+                    </div>
                 </div>
             </div>
             <div className={styles.box}>
-                <div className={styles.topContainer}><Image src="/assets/home/desktop/illustration-resourceful.svg" alt="open menu icon" width="202" height="202" /></div>
+                <div className={styles.topContainer}><Image src="/assets/shared/desktop/illustration-australia.svg" alt="location icon" width="202" height="202" /></div>
                 <div className={styles.bottomContainer}>
                     <h3>AUSTRALIA</h3>
-                    <a>SEE LOCATION</a>
+                    <div>
+                       <Link href="/locations"><a>SEE LOCATION</a></Link> 
+                    </div>
                 </div>
             </div>
             <div className={styles.box}>
-                <div className={styles.topContainer}><Image src="/assets/home/desktop/illustration-friendly.svg" alt="open menu icon" width="202" height="202" /></div>
+                <div className={styles.topContainer}><Image src="/assets/shared/desktop/illustration-united-kingdom.svg" alt="location icon" width="202" height="202" /></div>
                 <div className={styles.bottomContainer}>
                     <h3>UNITED KINGDOM</h3>
-                    <a>SEE LOCATION</a>
+                    <div>
+                       <Link href="/locations"><a>SEE LOCATION</a></Link> 
+                    </div>
                 </div>
             </div> 
         </div>
