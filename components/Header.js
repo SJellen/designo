70c35591000eb618dbf5d72064cde54bd40c14d6
SJellen <sjellen@me.com>
2021-09-20 13:36:33 -0400

    mobile nav finished

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
+import React, {useState} from 'react'
 import styles from '../styles/Header.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Header() {
+
+    const [isNavOpen, setIsNavOpen] = useState(false)
+
+    function handleClick() {
+        setIsNavOpen((prevState) => !prevState)
+    }
 
     return (
         <header className={styles.container}>
@@ -14,14 +21,18 @@ export default function Header() {
             <Link href="/locations" ><a >LOCATIONS</a></Link>
             <Link href="/contact" ><a >CONTACT</a></Link>
         </nav>
-        <nav className={styles.mobileNav}>
+        <nav className={styles.mobileNav} style={{display: isNavOpen ? "" : "none"}}>
             <Link href="/about" ><a >OUR COMPANY</a></Link>
             <Link href="/locations" ><a >LOCATIONS</a></Link>
             <Link href="/contact" ><a >CONTACT</a></Link>
         </nav>
         <div className={styles.mobileContainer}>
-            <Image src="/assets/shared/mobile/icon-hamburger.svg" alt="open menu icon" width="24" height="20" />
-            <Image src="/assets/shared/mobile/icon-close.svg" alt="close menu icon" width="20" height="20" /> 
+            <div style={{display: isNavOpen ? "none" : ""}} onClick={handleClick}>
+                <Image src="/assets/shared/mobile/icon-hamburger.svg" alt="open menu icon" width="24" height="20"  />
+            </div>
+            <div style={{display: isNavOpen ? "" : "none"}} onClick={handleClick}>
+               <Image src="/assets/shared/mobile/icon-close.svg" alt="close menu icon" width="20" height="20"  />  
+            </div>
         </div>
         </header>
     )
