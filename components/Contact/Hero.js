7ba796260b3d6ce88e8a22fe6b4e39990a57ce08
SJellen <sjellen@me.com>
2021-09-20 13:44:36 -0400

    hero text layouts

--- components/Contact/Hero.js
+++ components/Contact/Hero.js
@@ -4,6 +4,8 @@ export default function About() {
 
     return (
         <div className={styles.container}>
+            <h2>Contact Us</h2>
+            <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p>
            
         </div>
     )
