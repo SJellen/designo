b7485f2c58db7bdef851262a62f565c09c636fcf
SJellen <sjellen@me.com>
2021-09-23 14:29:34 -0400

    added background image on hero container

--- components/About/Hero.js
+++ components/About/Hero.js
@@ -1,5 +1,4 @@
 import styles from '../../styles/About/Hero.module.scss'
-import Image from 'next/image'
 
 export default function About() {
 
@@ -10,9 +9,7 @@ export default function About() {
                 <p>Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences.</p>
             </div>
             <div className={styles.imgContainer}>
-                <Image src="/assets/about/desktop/image-about-hero.jpg" alt="speaker" width="476" height="480"  className={styles.img}/>
             </div>
-           
         </div>
     )
 }
