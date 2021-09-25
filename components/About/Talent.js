efa6086b62079812d2a6e89ff6e677eee7fc4488
SJellen <sjellen@me.com>
2021-09-25 11:53:10 -0400

    start and layout for About/Talent component

---
+++ components/About/Talent.js
@@ -0,0 +1,14 @@
+import styles from '../../styles/About/Talent.module.scss'
+
+export default function About() {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.imgContainer}></div>
+            <div className={styles.textContainer}>
+                <h2>World-class talent</h2>
+                <p>We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.<br/><br/>Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
+            </div>
+        </div>
+    )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
 import styles from '../styles/About/About.module.scss'
 import Hero from '../components/About/Hero'
 import LocationNav from '../components/LocationNav'
+import Talent from '../components/About/Talent'
 
 export default function About() {
 
     return (
         <div className={styles.container}>
             <Hero />
+            <Talent />
             <LocationNav />
         </div>
     )
