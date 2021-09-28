a175a7384a4ef0d92ba469ec6a2f12e2db181a91
SJellen <sjellen@me.com>
2021-09-28 10:18:58 -0400

    work on Contact/Hero

--- components/Contact/Hero.js
+++ components/Contact/Hero.js
@@ -4,11 +4,11 @@ export default function About() {
 
     return (
         <div className={styles.container}>
-            <div>
+            <div className={styles.textContainer}>
                <h2>Contact Us</h2>
                 <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p> 
             </div>
-            <div>
+            <div className={styles.form}>
                 <input></input>
                 <input></input>
                 <input></input>
