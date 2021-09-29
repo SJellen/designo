d7fb59d9bce436294fc769b0692d8306d50f537c
SJellen <sjellen@me.com>
2021-09-29 12:16:57 -0400

    work on form styling

--- components/Contact/Hero.js
+++ components/Contact/Hero.js
@@ -9,10 +9,10 @@ export default function About() {
                 <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.</p> 
             </div>
             <div className={styles.form}>
-                <input placeholder="name" />
-                <input placeholder="email" />
-                <input placeholder="phone" />
-                <textarea />
+                <input placeholder="name" id="name" className="name"/>
+                <input placeholder="email" id="email" className="email"/>
+                <input placeholder="phone" id="phone" className="phone"/>
+                <textarea placeholder="message" className="textarea"/>
             </div>
         </div>
     )
