a345cdc4e4b9efcabf9724f1ccee6b28bb6eb8ab
SJellen <sjellen@me.com>
2021-10-15 10:59:58 -0400

    work to set up leaflet

--- components/Locations/TopContainer.js
+++ components/Locations/TopContainer.js
@@ -3,8 +3,11 @@ import styles from '../../styles/Locations/TopContainer.module.scss'
 
 export default function TopContainer() {
 
-
+    
     return (
-        <div className={styles.container}>TopContainer</div>
+        <div className={styles.container}>
+            <div className={styles.info}></div>
+            <div className={styles.map}>{myMap}</div>
+        </div>
     )
 }
