4f4f7472b0268c4b641a5c2ec21a1d7ab3ee592b
SJellen <sjellen@me.com>
2021-12-26 11:03:14 -0500

    whitespace removal

--- components/Home/WebDesignTileHome.js
+++ components/Home/WebDesignTileHome.js
@@ -18,6 +18,4 @@ export default function WebDesignTileHome() {
      </div>  
      </Link>
     )
-
-
 }
--- components/ImageNavTile/GraphicDesign.js
+++ components/ImageNavTile/GraphicDesign.js
@@ -18,6 +18,4 @@ export default function GraphicDesignTile() {
        </div>  
        </Link>
     )
-
-
 }
