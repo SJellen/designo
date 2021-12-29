5d6dd9bbe95906a9f8dbb71a6d72a00eba3a5640
SJellen <sjellen@me.com>
2021-12-29 09:49:06 -0500

    hero container spacing, get in touch display

--- components/GetInTouch.js
+++ components/GetInTouch.js
@@ -1,11 +1,14 @@
 import styles from "../styles/GetInTouch.module.scss"
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function GetInTouch() {
 
+    const { asPath } = useRouter()
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={{display: asPath === "/contact" ? 'none' : '' }}>
             <div className={styles.flexContainer}>  
                 <div className={styles.leftContainer}>
                     <h1>Let’s talk about your project</h1>
